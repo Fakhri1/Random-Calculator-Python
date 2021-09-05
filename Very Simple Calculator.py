904620b41a20bdfd85160cf9717968eca61f284b
Fakhri F <44204754+Fakhri1@users.noreply.github.com>
2021-09-05 15:38:43 +0700

    Very Simple Calculator v0.5

--- Very Simple Calculator.py
+++ Very Simple Calculator.py
@@ -1,20 +1,62 @@
-print("Kalukulator Sangat Sederhana di Python")
+print("SELAMAT DATANG DI KALKULATOR SEDERHANA Powered by Python")
 print("")
 
-def LIST() :
-    Menu = ["1. Penjumlahan","2. Pengurangan","3. Perkalian","4. Pembagian","5. Versi Aplikasi","6. Keluar"]
+def Menu_List() :
+    print("")
+    print("MENU UTAMA")
+    print("")
+    Menu = ["1. Kalkulator" , "2. Temperatur" , "3. Versi Aplikasi","4. Keluar"]
+    print(Menu[0])
+    print(Menu[1])
+    print(Menu[2])
+    print(Menu[3])
+    print("")
+    Menu_Utama()
+
+def Calculate_List():
+    print("")
+    print("MENU KALKULATOR")
+    print("")
+    Menu = ["1. Penjumlahan","2. Pengurangan","3. Perkalian","4. Pembagian","0. Kembali Ke Menu Utama"]
     print(Menu[0])
     print(Menu[1])
     print(Menu[2])
     print(Menu[3])
     print(Menu[4])
-    print(Menu[5])
     print("")
-    menu()
+    Menu_Kalkulator()
 
-
-def menu() :
+def Temperatur_List():
     print("")
+    print("KALKULATOR SUHU")
+    print("")
+    Menu = ["1. Celcius / C","2. Reamur / R","3. Kelvin / K","4. Fahrenheit / F","0. Kembali ke Menu Utama"]
+    print(Menu[0])
+    print(Menu[1])
+    print(Menu[2])
+    print(Menu[3])
+    print(Menu[4])
+    print("")
+    Temperatur()
+
+def Menu_Utama():
+    mainmenu = input("Silahkan tulis angka sesuai keterangan di atas! : ")
+    if mainmenu == "1" :
+        Calculate_List()
+    else :
+        if mainmenu == "2" :
+            Temperatur_List()
+        else :
+            if mainmenu == "3" :
+                versi()
+            else :
+                if mainmenu == "4" :
+                    exit
+                else : 
+                    print("Angka tidak terdefinisi. Mohon menulis angka yang sudah disediakan!")
+                    Menu_List()
+
+def Menu_Kalkulator() :
     operasi = input("Silahkan tulis angka sesuai keterangan di atas! : ")
     if operasi == "1":
         penjumlahan()
@@ -28,19 +70,48 @@ def menu() :
                 if operasi == "4":
                     pembagian()
                 else:
-                    if operasi == "5":
-                        versi()
+                    if operasi == "0":
+                        Menu_List
                     else:
-                        if operasi == "6":
-                            exit
-                        else:
-                            if operasi == "LIST":
-                                LIST()
-                            else:
-                                print("Angka tidak terdefinisi. Mohon menulis angka yang sudah disediakan!")
-                                print("Silahkan ketik - LIST - apabila Anda lupa dengan angkanya!")
-                                print("")
-                                menu()
+                        print("Angka tidak terdefinisi. Mohon menulis angka yang sudah disediakan!")
+                        print("")
+                        Calculate_List()
+
+def Temperatur() :
+    print("")
+    suhu = input("Silahkan tulis nomer sesuai dengan jenis suhu yang diketahui : ")
+    print("")
+    if suhu == "1" :
+        a = input("Masukkan Angka Suhunya! : ")
+        print (a, " Celcius = ", (int(a)*(4/5)) , " Reamur = " , (int(a)+273) , " Kelvin = " , ((int(a)*(9/5))+32) , " Fahrenheit" )
+        print("")
+        akhir()
+    else :
+        if suhu == "2" :
+            a = input("Masukkan Angka Suhunya! : ")
+            print (a, " Reamur = ",(int(a)*(5/4))," Celcius = ",((int(a)*(5/4))+273)," Kelvin = ",((int(a)*(9/4))+32)," Fahrenheit")
+            print("")
+            akhir()
+        else :
+            if suhu == "3" :
+                a = input("Masukkan Angka Suhunya! : ")
+                print(a, " Kelvin = ",(273-(int(a)))," Celcius = ",((int(a)-273)*(4/5))," Reamur = ",((9/5)*((int(a)-273)+32))," Fahrenheit" )
+                print("")
+                akhir()
+            else :
+                if suhu == "4" :
+                    a = input("Masukkan Angka Suhunya! : ")
+                    print(a," Fahrenheit = ",((5/9)*(int(a)-32))," Celcius = ",((4/9)*(int(a)-43)), " Reamur = ",((5/9)*(int(a)-32)+273), " Kelvin" )
+                    print("")
+                    akhir()
+                else :
+                    if suhu == "0" :
+                        Menu_List()
+                    else :
+                        print("Angka tidak terdefinisi. Mohon menulis angka yang sudah disediakan!")
+                        print("")
+                        Temperatur_List()
+
 
 def penjumlahan() :
     a = input("Masukkan angka pertama : ")
@@ -74,7 +145,7 @@ def akhir() :
     x = input("Kembali ke Menu Awal? Y/N : ")
     if x == "Y":
         print("")
-        menu()
+        Menu_List()
     else:
         if x == "N" :
             exit
@@ -84,12 +155,14 @@ def akhir() :
             akhir()
 
 def versi() :
-    print("Versi Aplikasi : v.0.4")
+    print("Versi Aplikasi : v.0.6")
     print("Last Updated : 2021-09-05")
     print("Created by FakhriF")
     print("")
+    print("Silahkan cek versi terbaru aplikasi ini di Github.com/Fakhri1/Random-Calculator-Python !")
+    print("")
     akhir()
         
-LIST()
+Menu_List()
 
 exit = print("Program Terminated")
